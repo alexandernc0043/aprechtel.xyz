ddb3ccb76a129ccb6441917b75bc5cb825f85893
Alexander Prechtel <aprechtel0043@gmail.com>
2025-02-13 00:26:32 -0500

    Removed JS, Changed Footer

--- scripts/homepage.js
+++
@@ -1,9 +0,0 @@
-// Initial check
-document.addEventListener('DOMContentLoaded', function() {
-    const footer = document.querySelector('footer');
-    if (window.innerWidth < 768) {
-        footer.innerHTML = `<p>Created by Alexander Prechtel<br><a href="https://www.linkedin.com/in/alexander-prechtel-b4a0a9283/" target="_blank">LinkedIn</a> || <a href="https://github.com/alexandernc0043" target="_blank">Github</a> || <a href="https://github.com/alexandernc0043/aprechtel.xyz" target="_blank">Source Code</a></p>`
-    } else {
-        footer.innerHTML = `<p>Created by Alexander Prechtel || <a href="https://www.linkedin.com/in/alexander-prechtel-b4a0a9283/" target="_blank">LinkedIn</a> || <a href="https://github.com/alexandernc0043" target="_blank">Github</a> || <a href="https://github.com/alexandernc0043/aprechtel.xyz" target="_blank">Source Code</a></p>`
-    }
-});
